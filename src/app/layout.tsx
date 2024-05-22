4ec335f5eefb440207274942b801018e74a23c99
Dani Servian <daniserviandev@gmail.com>
2024-05-22 15:02:03 +0200

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+
+// Vercel Analytics
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,14 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className + "h-screen bg-fixed bg-gradient-to-t from-orange-500/10 ..."}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
