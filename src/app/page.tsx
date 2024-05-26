c7399df10ec50b8c04f994ed062043aa68f540ef
Dani Servian <daniserviandev@gmail.com>
2024-05-26 12:58:49 +0200

    try

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,25 @@
-export const revalidate = 120;
 
 import { ModeToggle } from "@/components/ui/dark-mode-toggle";
 import { columns } from "./pools/columns";
 import { DataTable } from "./pools/data-table";
 import Goose from "@/components/ui/goose";
 import { getCombinedPairs } from "@/app/pools/dexscreener";
+import { CombinedPair } from './pools/interfaces';
 
-async function getData() {
-
-  try {
-    const combinedPairs = await getCombinedPairs();
-    return combinedPairs;
-  } catch (error) {
-    console.error('Error:', error);
-    return [];
-  }
-}
+export const revalidate = 120; // Revalidate every 120 seconds
 
 export default async function DemoPage() {
-  const data = await getData();
+  let data: CombinedPair[] = [];
+
+  try {
+    data = await getCombinedPairs();
+  } catch (error) {
+    console.error('Error:', error);
+  }
 
   return (
     <div className="container mx-auto py-10">
       <div className="flex items-center justify-between space-x-2 py-4">
-        {/* Meteora gradient: 87deg, #FF2189 -36.79%, #FF9D00 114.4%*/}
         <div>
           <h1 className="text-2xl font-bold inline-block text-transparent bg-clip-text md:text-3xl"
             style={{ backgroundImage: 'linear-gradient(87deg, #FF2189 -36.79%, #FF9D00 114.4%)' }}>
@@ -38,7 +34,7 @@ export default async function DemoPage() {
           <span className="mr-1">Made for the Meteora community,</span>
           <span>with love by <span className="font-bold text-transparent bg-clip-text"
             style={{ backgroundImage: 'linear-gradient(87deg, #FF2189 -36.79%, #FF9D00 114.4%)' }}>Diisaster</span>.</span>
-            <Goose/>
+          <Goose />
         </div>
         <div>2024</div>
       </footer>
--- src/app/pools/data-table.tsx
+++ src/app/pools/data-table.tsx
@@ -1,145 +1,143 @@
-"use client"
+// src/app/pools/data-table.tsx
 
-import { Input } from "@/components/ui/input"
-import * as React from "react"
-import {
-    ColumnDef,
-    SortingState,
-    ColumnFiltersState,
-    flexRender,
-    getFilteredRowModel,
-    getCoreRowModel,
-    getSortedRowModel,
-    getPaginationRowModel,
-    useReactTable,
-} from "@tanstack/react-table"
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
-import { DataTablePagination } from "./data-table-pagination"
-import { useEffect } from "react"
-import DataTableFilters from "./data-table-filters"
-import { Label } from "@/components/ui/label"
+"use client";
 
-interface DataTableProps<TData, TValue> {
-    columns: ColumnDef<TData, TValue>[]
-    data: TData[]
+import { Input } from "@/components/ui/input";
+import * as React from "react";
+import {
+  ColumnDef,
+  SortingState,
+  ColumnFiltersState,
+  flexRender,
+  getFilteredRowModel,
+  getCoreRowModel,
+  getSortedRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { DataTablePagination } from "./data-table-pagination";
+import { useEffect } from "react";
+import { Label } from "@/components/ui/label";
+import { CombinedPair } from "./interfaces";
+
+interface DataTableProps {
+  columns: ColumnDef<CombinedPair, any>[];
+  data: CombinedPair[];
 }
 
-export function DataTable<TData, TValue>({
+export function DataTable({ columns, data }: DataTableProps) {
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const table = useReactTable({
+    data,
     columns,
-    data
-}: DataTableProps<TData, TValue>) {
-    const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-        []
-    );
-    const [sorting, setSorting] = React.useState<SortingState>([]);
-    const table = useReactTable({
-        data,
-        columns,
-        getCoreRowModel: getCoreRowModel(),
-        getPaginationRowModel: getPaginationRowModel(),
-        onColumnFiltersChange: setColumnFilters,
-        getFilteredRowModel: getFilteredRowModel(),
-        onSortingChange: setSorting,
-        getSortedRowModel: getSortedRowModel(),
-        state: {
-            columnFilters,
-            sorting,
-        },
-    });
-    useEffect(() => {
-        table.setPageSize(30);
-        table.getColumn("liquidity")?.toggleSorting(true);
-    }, [table]);
-    return (
-        <div>
-            <div className="flex flex-wrap items-center justify-between py-3 w-full">
-                <div className="flex flex-wrap items-center justify-between py-3 w-full md:w-auto">
-                    <div className="flex items-center py-3">
-                        <Label htmlFor="name" className="whitespace-nowrap text-sm mr-2">Name</Label>
-                        <Input
-                            placeholder="Filter by name..."
-                            value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
-                            onChange={(event) =>
-                                table.getColumn("name")?.setFilterValue(event.target.value)
-                            }
-                            className="w-32 mr-5 sm:w-64"
-                            id="name"
-                            autoFocus
-                        />
-                    </div>
-                    {/* <DataTableFilters table={table} /> */}
-                    <div className="flex items-center py-3">
-                        <Label htmlFor="min-liq" className="whitespace-nowrap text-sm mr-2">Min $TVL</Label>
-                        <Input
-                            type="number"
-                            placeholder="0"
-                            value={(table.getColumn("liquidity")?.getFilterValue() as string) ?? ""}
-                            onChange={(event) =>
-                                table.getColumn("liquidity")?.setFilterValue(event.target.value)
-                            }
-                            id="min-liq"
-                            className="w-16 mr-5 sm:w-32"
-                        />
-                    </div>
-                </div>
-                <div className="ml-auto mt-3 md:mt-0">
-                    <DataTablePagination table={table} />
-                </div>
-            </div>
-            <div className="rounded-md border">
-                <Table>
-                    <TableHeader>
-                        {table.getHeaderGroups().map((headerGroup) => (
-                            <TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map((header) => {
-                                    return (
-                                        <TableHead key={header.id}>
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(
-                                                    header.column.columnDef.header,
-                                                    header.getContext()
-                                                )}
-                                        </TableHead>
-                                    )
-                                })}
-                            </TableRow>
-                        ))}
-                    </TableHeader>
-                    <TableBody>
-                        {table.getRowModel().rows?.length ? (
-                            table.getRowModel().rows.map((row) => (
-                                <TableRow
-                                    key={row.id}
-                                    data-state={row.getIsSelected() && "selected"}
-                                >
-                                    {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
-                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            ))
-                        ) : (
-                            <TableRow>
-                                <TableCell colSpan={columns.length} className="h-24 text-center">
-                                    No results.
-                                </TableCell>
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            </div>
-            <div className="flex items-center justify-end space-x-2 py-4">
-                <DataTablePagination table={table} />
-            </div>
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      columnFilters,
+      sorting,
+    },
+  });
+
+  useEffect(() => {
+    table.setPageSize(30);
+    table.getColumn("liquidity")?.toggleSorting(true);
+  }, [table]);
+
+  return (
+    <div>
+      <div className="flex flex-wrap items-center justify-between py-3 w-full">
+        <div className="flex flex-wrap items-center justify-between py-3 w-full md:w-auto">
+          <div className="flex items-center py-3">
+            <Label htmlFor="name" className="whitespace-nowrap text-sm mr-2">Name</Label>
+            <Input
+              placeholder="Filter by name..."
+              value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+              onChange={(event) =>
+                table.getColumn("name")?.setFilterValue(event.target.value)
+              }
+              className="w-32 mr-5 sm:w-64"
+              id="name"
+              autoFocus
+            />
+          </div>
+          <div className="flex items-center py-3">
+            <Label htmlFor="min-liq" className="whitespace-nowrap text-sm mr-2">Min $TVL</Label>
+            <Input
+              type="number"
+              placeholder="0"
+              value={(table.getColumn("liquidity")?.getFilterValue() as string) ?? ""}
+              onChange={(event) =>
+                table.getColumn("liquidity")?.setFilterValue(event.target.value)
+              }
+              id="min-liq"
+              className="w-16 mr-5 sm:w-32"
+            />
+          </div>
         </div>
-    )
+        <div className="ml-auto mt-3 md:mt-0">
+          <DataTablePagination table={table} />
+        </div>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <DataTablePagination table={table} />
+      </div>
+    </div>
+  );
 }
